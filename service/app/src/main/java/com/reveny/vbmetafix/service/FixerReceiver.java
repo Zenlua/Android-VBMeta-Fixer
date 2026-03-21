8c0f5cde391c5d80b1d935be491a3b5485772478
Kakathic <kakathic@gmail.com>
2026-03-21 12:32:45 +0700

    Update FixerReceiver.java

--- service/app/src/main/java/com/reveny/vbmetafix/service/FixerReceiver.java
+++ service/app/src/main/java/com/reveny/vbmetafix/service/FixerReceiver.java
@@ -14,20 +14,28 @@ public class FixerReceiver extends BroadcastReceiver {
 
     private static final String TAG = "FixerReceiver";
 
-    @Override
+     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.e(TAG, "Receiver triggered");
+    
+        final PendingResult result = goAsync();
+    
         new Thread(() -> {
             try {
                 String bootHash = Entry.run();
-                Log.d(TAG, "Boot hash: " + bootHash);
-
+                Log.e(TAG, "Boot hash: " + bootHash);
+    
                 File file = new File(context.getCacheDir(), "boot.hash");
                 try (FileOutputStream fos = new FileOutputStream(file)) {
                     fos.write(bootHash.getBytes());
                 }
-
+    
+                Log.e(TAG, "File written: " + file.getAbsolutePath());
+    
             } catch (Exception e) {
                 Log.e(TAG, "Error", e);
+            } finally {
+                result.finish(); // 🔥 cực kỳ quan trọng
             }
         }).start();
     }
